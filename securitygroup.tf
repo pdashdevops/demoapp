01339afd0eccaaab55a7945d3095fdfe7eb86154
pdashdevops <89957542+pdashdevops@users.noreply.github.com>
2023-02-04 10:52:10 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -2,7 +2,7 @@ module "ssh_service_sg_private" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC"
+  description = "Security group for Private Instances SG - Allow all incoming traffic from within VPC and all egress"
   vpc_id      = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = [
@@ -21,13 +21,32 @@ module "ssh_service_sg_public" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC"
+  description = "Security group for Bastion host SG - Allow self ip to ssh to ‘bastion’ instance and allow all egress."
   vpc_id      = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = [
     {
       from_port   = 22
       to_port     = 22
+      protocol    = "tcp"
+      description = "ssh-service ports"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_rules = ["all-all"]
+}
+  
+module "ssh_service_sg_public_web" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group for Public Web SG - Allow incoming to port 80 from self IP and all egress."
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
       protocol    = "tcp"
       description = "public-service ports"
       cidr_blocks = "0.0.0.0/0"
